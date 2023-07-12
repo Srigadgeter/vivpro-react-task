5052f6fc429723269ef4ac5ed69ba06f8a5df591
Srigadgeter <srigadgets@gmail.com>
2023-07-12 18:57:23 +0530

    consumed the SongsTable in the App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+// external imports
 import React from "react";
+
+// internal imports
+import SongsTable from "./components/SongsTable";
+
+// styles
 import "./App.css";
 
 function App() {
-  return <div className="App">React App</div>;
+  return (
+    <div className="App">
+      <SongsTable />
+    </div>
+  );
 }
 
 export default App;
