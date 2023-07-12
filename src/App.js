cee4916dbe615b16b99c23861a001d2e0c196e3d
Srigadgeter <srigadgets@gmail.com>
2023-07-12 22:51:01 +0530

    error message mapping

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const App = () => {
         setShowToaster(true);
         setToastInfo({
           variant: "error",
-          message: error
+          message: error?.message || "Something went wrong"
         });
       })
       .finally(() => setLoader(false));
