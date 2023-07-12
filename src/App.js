c0540ec5c0d7dc2428c11124d40e94e5e6a306f4
Srigadgeter <srigadgets@gmail.com>
2023-07-12 20:41:23 +0530

    updated App component with the search logic (incl. api call)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-/* eslint-disable no-console */
 // external imports
 import React, { useEffect, useState } from "react";
 
 // internal imports
+import Header from "./components/Header";
 import SongsTable from "./components/SongsTable";
 
 // styles
@@ -10,22 +10,54 @@ import "./App.css";
 
 const App = () => {
   const [rows, setRows] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [isSearched, setIsSearched] = useState(false);
+  const [loader, setLoader] = useState(false);
 
-  useEffect(() => {
-    fetch(`http://localhost:8000/playlist`)
+  const fetchData = (title = "") => {
+    setLoader(true);
+    const url = `http://localhost:8000/playlist${title ? `?title=${title}` : ""}`;
+    fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        console.log("data >>", data);
+        // console.log("data >>", data);
         setRows(data);
       })
       .catch((error) => {
         console.error("Error >>", error);
-      });
-  }, []);
+      })
+      .finally(() => setLoader(false));
+  };
+
+  useEffect(() => fetchData(), []);
+
+  // Handler helps to record the changes of the search field
+  const handleChange = ({ target: { value } }) => setSearchText(value);
+
+  // Handler helps to fetch backend data based on searchText
+  const handleSearch = () => {
+    setIsSearched(true);
+    fetchData(searchText);
+  };
+
+  // Handler helps to clear the search
+  const handleClearSearch = () => {
+    setSearchText("");
+    if (isSearched) {
+      setIsSearched(false);
+      fetchData();
+    }
+  };
 
   return (
     <div className="App">
-      <SongsTable rows={rows} />
+      <Header
+        searchText={searchText}
+        handleChange={handleChange}
+        handleSearch={handleSearch}
+        handleClearSearch={handleClearSearch}
+      />
+      <SongsTable rows={rows} loader={loader} />
     </div>
   );
 };
